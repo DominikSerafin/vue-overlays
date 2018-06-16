f9c6e78c9d1b05592daa5bec02c70c4a234dc7f7
DominikSerafin <DominikSerafin@users.noreply.github.com>
2018-06-16 22:24:33 +0200

    Trying to export...

--- README.md
+++ README.md
@@ -3,3 +3,20 @@
 Vue Overlays (Portal, Modal, Popover) based on React-Overlays and React Material-UI 1.X
 
 Work in progress
+
+
+## TODO:
+
+* Good transitions support
+* Fix hackish Portal beforeDestroy cleanup
+* Refactor/improve sitauation around zIndex/'mui-fixed'/scrollbars/hidden overflow
+* Make ModalManager scoped the same way as in MUI version
+* Good SRR support
+* Good way to customize styling/classes of VO components
+* Passing DOM attributes like aria-* to VO components
+* Sinking props into lower level components (e.g. Popover props => Modal props)
+* Modal: keepMounted
+* Modal: BackdropComponent
+* Modal: children props role
+* Modal: tabIndex prop make it work similar as in React version render
+* Backdrop: custom backdrop rendering (slot?)
--- src/index.js
+++ src/index.js
@@ -1,57 +1,43 @@
-/*
-  TODO:
-
-  * Good transitions support
-  * Fix hackish Portal beforeDestroy cleanup
-  * Refactor/improve sitauation around zIndex/'mui-fixed'/scrollbars/hidden overflow
-  * Make ModalManager scoped the same way as in MUI version
-  * Good SRR support
-  * Good way to customize styling/classes of VO components
-  * Passing DOM attributes like aria-* to VO components
-  * Sinking props into lower level components (e.g. Popover props => Modal props)
-  * Modal: keepMounted
-  * Modal: BackdropComponent
-  * Modal: children props role
-  * Modal: tabIndex prop make it work similar as in React version render
-  * Backdrop: custom backdrop rendering (slot?)
-
-*/
-
-
-
-
 import Portal from './Portal.js';
 import Backdrop from './Backdrop.js';
 import Modal from './Modal.js';
+import ModalManager from './ModalManager.js';
 import Popover from './Popover.js';
 
 
-//if (typeof window !== typeof void 0){
-  window.vo = {
+var publicExport = function(){
+  return {
     Portal: Portal,
     Backdrop: Backdrop,
     Modal: Modal,
+    ModalManager: ModalManager,
     Popover: Popover,
   }
-//};
+}
 
 
-
-
-  /*------------------------------------*\
-    Expose / Autoinstall
-  \*------------------------------------*/
-  /*
-  if (typeof exports === 'object' && typeof module === 'object') {
-    module.exports = XXXXX;
-  } else if (typeof define === 'function' && define.amd) {
-    define(function () { return XXXXX });
-  } else if (typeof window !== typeof void 0) {
-    window.XXXXX = XXXXX;
-  }
-
-  if (typeof Vue !== typeof void 0) {
-    Vue.use(XXXXX);
-  }
-
+/*
+if (typeof exports === 'object' && typeof module === 'object') {
+  console.warn(1);
+  module.exports = publicExport();
+} else if (typeof define === 'function' && define.amd) {
+  console.warn(2);
+  define(function () { return publicExport() });
+} else if (typeof window !== typeof void 0) {
+  console.warn(3);
+  window.vo = publicExport();
+}
 */
+
+/*
+if (typeof Vue !== typeof void 0) {
+  Vue.use(XXXXX);
+}
+*/
+
+
+
+window.vo = publicExport();
+
+
+module.exports = module.exports.default = publicExport();
