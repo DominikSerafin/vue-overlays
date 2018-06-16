7c0a58b9fe13ec59e2c5c1e5e3d76f0fd5021610
DominikSerafin <DominikSerafin@users.noreply.github.com>
2018-06-16 22:49:42 +0200

    Debuggin exporting

--- src/index.js
+++ src/index.js
@@ -5,39 +5,16 @@ import ModalManager from './ModalManager.js';
 import Popover from './Popover.js';
 
 
-var publicExport = function(){
-  return {
-    Portal: Portal,
-    Backdrop: Backdrop,
-    Modal: Modal,
-    ModalManager: ModalManager,
-    Popover: Popover,
-  }
+var publicExport = {
+  Portal: Portal,
+  Backdrop: Backdrop,
+  Modal: Modal,
+  ModalManager: ModalManager,
+  Popover: Popover,
 }
 
 
-/*
-if (typeof exports === 'object' && typeof module === 'object') {
-  console.warn(1);
-  module.exports = publicExport();
-} else if (typeof define === 'function' && define.amd) {
-  console.warn(2);
-  define(function () { return publicExport() });
-} else if (typeof window !== typeof void 0) {
-  console.warn(3);
-  window.vo = publicExport();
-}
-*/
-
-/*
-if (typeof Vue !== typeof void 0) {
-  Vue.use(XXXXX);
-}
-*/
+window.vo = publicExport;
 
 
-
-window.vo = publicExport();
-
-
-module.exports = module.exports.default = publicExport();
+module.exports = publicExport;
